35ab88fb2b91a5ba60dd769467c06e0176f62288
Vikas <vikasdhotre2020@gmail.com>
2026-09-13 01:36:23 +0530

    this is fourth commit

---
+++ const.js
@@ -0,0 +1,12 @@
+
+const weekTotalDays = 7;// declaration + initialization
+console.log("Total days in a week:", weekTotalDays);
+    
+// Reassignment is not allowed for const variables
+
+//weekTotalDays = 8; // Reassignment or reinitialization is not allowed for const variables
+
+//const weekTotalDays = 10; // Redeclaration is not allowed for const variables
+
+//“A const variable must be initialized at the time of declaration. 
+// Reassignment and redeclaration are not allowed in the same scope.”
