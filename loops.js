f1bcd8d7e4eb8738aa4907df01adc497c40c0547
Vikas <vikasdhotre2020@gmail.com>
2026-09-14 11:55:32 +0530

    this commit is modified i am added new lines for testing update

--- loops.js
+++ loops.js
@@ -32,3 +32,70 @@ while (i <= 5) {
     console.log(i);
     i++;
 }
+
+// example of while loop in real automation testing 
+//Jab tak condition true hai, while loop repeatedly execute hota hai.
+
+let retryCount = 1;
+while (retryCount <= 3) {
+    console.log("Retrying to execute the test case, attempt number: " + retryCount);
+    retryCount++;
+}
+
+//3 do while loop
+//do...while mein code kam se kam ek baar execute hota hi hai, uske baad condition check hoti hai.
+
+let j = 10;
+do {
+    console.log("value of j:", j);
+    j++;
+}while (j <= 5);
+
+// testing usage Kisi operation ko at least once perform karna ho, phir condition check karni ho.
+
+
+// 4 for...of loop
+// for...of ka use iterable values jaise arrays aur strings ke individual values ko iterate karne ke liye hota hai.
+
+let browsers = ["Chrome", "Firefox", "Edge", "Safari"];
+for (let browser of browsers) {
+    console.log("Testing on browser: " + browser);
+}
+
+//Cross-browser testing mein: use hota hai
+let browser = ["Chrome", "Firefox", "Edge", "Safari"];
+for (let browser of browsers) {
+    console.log("Running test cases on: " + browser);
+}
+
+// 5 for...in loop
+//for...in ka use object ki keys/properties ko iterate karne ke liye hota hai.
+//API response/object ke fields inspect karne mein useful ho sakta hai.
+
+let user = {
+    name: "Vikas D",
+    role: "SDET",
+    experience: 5
+};
+ for (let key in user) {
+    console.log(key + ": " + user[key]);
+}
+
+//6 break loop ko immediately stop kar deta hai.
+for (let i = 1; i <= 10; i++) {
+    if (i === 5) {
+        break; // loop ko stop kar do
+    }
+    console.log(i);
+}
+
+//Testing usage
+//Suppose multiple test cases execute kar rahe ho aur critical failure mil gaya:
+
+for (let testCase = 1; testCase <= 10; testCase++) {
+    if (testCase === 5) {
+        console.log("Critical failure in test case 5. Stopping further execution.");
+        break; // loop ko stop kar do   
+    }
+    console.log("Executing test case number: " + testCase);
+}
